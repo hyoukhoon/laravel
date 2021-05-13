5dab68eb9c31e69590d8c365c9090d9dcec61193
hyoukhoon <hyoukhoon@cnspartner.com>
2021-05-13 15:08:54 +0900

    customer

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,9 @@ Route::get('/', function () {
     ]);
 });
 
+Route::resource('customers','CustomerController');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
